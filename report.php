dd0cbe34f2d5babe16ab3fb4355d2b515ed663d6
Nelson Love <nelson.love@me.com>
2013-02-18 04:51:12 -0500

    Fixed boolean logic on counter checks
    Event counter and scheduled days counter was borked, should be fixed now

--- report.php
+++ report.php
@@ -116,7 +116,7 @@
 				}
 				
 				# Now we echo the person's table if there have been events this week
-				if (($event_counter > 0) and ($scheduled_days == 0)) {
+				if (($event_counter > 0) or ($scheduled_days > 0)) {
 					echo "<h3>" . $person->name . " / " . $person->phone . " / " . $person->email . "</h3>" . "\n";
 					echo "<table>";
 					$tr = array("headings", "times", "in1", "out1", "in2", "out2");
